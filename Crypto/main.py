293b2362b0c4064f9feb0d82b276571bdbcedbb3
Jordan_Souop <souopjeff@gmail.com>
2023-12-30 01:01:28 +0100

    Final Version

--- Crypto/main.py
+++ Crypto/main.py
@@ -1,14 +1,17 @@
 from Crypto.batch_gcd import GCDProcessor
 from Crypto.calcul_module import CertificateProcessor
+from Crypto.crawler import CertificateFetcher
 from extract_certificates import CertificateExtractor
 from extract_keys import process_certificates
 from doublons import process
 
 
 def main():
-    extractor = CertificateExtractor('domain.txt', 'certificat.txt')
+    # extractor = CertificateExtractor('domain.txt', 'certificat.txt')
+    # extractor.extract_certificates()
 
-    extractor.extract_certificates()
+    num_certs = 10
+    CertificateFetcher.fetch_certificates(num_certs)
 
     process_certificates('certificat.txt', 'keys.txt')
 
